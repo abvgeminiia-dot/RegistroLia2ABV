4d43e29d7896e945c9e95ee2f8e60eeb8af37a47
David Murati <davidmurati1@gmail.com>
2025-10-15 16:57:44 -0400

    13 commit registroLiaABV

--- src/Component/FormABV1/FormABV2.jsx
+++ src/Component/FormABV1/FormABV2.jsx
@@ -390,7 +390,7 @@ const FormABV2 = () => {
                                     Abrir Correo para Adjuntar
                                 </a>
                                 <small className="form-text text-muted">
-                                    Por favor, envíe el RIF a: <strong>abv.gemini.ia@gmail.com</strong>
+                                    Por favor, en caso de que tenga habilitado protecciones en el navegador envíe el RIF a: <strong>abv.thap@asobanca.com.ve</strong>
                                 </small>
                             </div>
                         </div>
